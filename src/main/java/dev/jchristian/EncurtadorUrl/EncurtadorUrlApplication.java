c5cfb2f3cc4c417361b9a7c358fcfc89fa99d637
Jabes Christian <jabescristian@gmail.com>
2026-02-17 20:23:58 -0300

    feat: estrutura inicial da aplicação web de encurtador de url com spring

---
+++ src/main/java/dev/jchristian/EncurtadorUrl/EncurtadorUrlApplication.java
@@ -0,0 +1,13 @@
+package dev.jchristian.EncurtadorUrl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncurtadorUrlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncurtadorUrlApplication.class, args);
+	}
+
+}
