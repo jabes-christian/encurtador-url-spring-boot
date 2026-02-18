a023dbd01bb8e1fbfb34cb30230e8891cd1f3657
Jabes Christian <jabescristian@gmail.com>
2026-02-18 20:48:47 -0300

    Criação dos arquivos dinamicos usando Thymeleaf

--- src/main/java/dev/jchristian/EncurtadorUrl/EncurtadorUrlApplication.java
+++ src/main/java/dev/jchristian/EncurtadorUrl/EncurtadorUrlApplication.java
@@ -2,8 +2,9 @@ package dev.jchristian.EncurtadorUrl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class EncurtadorUrlApplication {
 
 	public static void main(String[] args) {
