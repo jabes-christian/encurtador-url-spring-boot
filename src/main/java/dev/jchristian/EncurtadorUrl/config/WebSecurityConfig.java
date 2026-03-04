e6a1da39c260a6b899d8ac55bc4cce08bc3cb7bf
Jabes Christian <jabescristian@gmail.com>
2026-03-03 21:07:08 -0300

    Ajuste de indexação

--- src/main/java/dev/jchristian/EncurtadorUrl/config/WebSecurityConfig.java
+++ src/main/java/dev/jchristian/EncurtadorUrl/config/WebSecurityConfig.java
@@ -21,26 +21,26 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(CsrfConfigurer::disable)
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(
-                                "/error", "/webjars/**", "/css/**", "/js/**", "/images/**",
-                                "/", "/short-urls", "/s/**", "/register", "/login"
-                        ).permitAll()
-                        .requestMatchers("/my-urls").authenticated()
-                        .requestMatchers("/admin/**").hasRole("ADMIN")
-                        .anyRequest().authenticated()
-                )
-                .formLogin(form -> form
-                        .loginPage("/login")
-                        .defaultSuccessUrl("/")
-                        .permitAll()
-                )
-                .logout(logout -> logout
-                        .logoutUrl("/logout")
-                        .logoutSuccessUrl("/login?logout")
-                        .permitAll()
-                );
+            .csrf(CsrfConfigurer::disable)
+            .authorizeHttpRequests(authorize -> authorize
+                    .requestMatchers(
+                            "/error", "/webjars/**", "/css/**", "/js/**", "/images/**",
+                            "/", "/short-urls", "/s/**", "/register", "/login"
+                    ).permitAll()
+                    .requestMatchers("/my-urls").authenticated()
+                    .requestMatchers("/admin/**").hasRole("ADMIN")
+                    .anyRequest().authenticated()
+            )
+            .formLogin(form -> form
+                    .loginPage("/login")
+                    .defaultSuccessUrl("/")
+                    .permitAll()
+            )
+            .logout(logout -> logout
+                    .logoutUrl("/logout")
+                    .logoutSuccessUrl("/login?logout")
+                    .permitAll()
+            );
 
         return http.build();
     }
